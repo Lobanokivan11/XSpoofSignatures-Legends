0fec319ce7d564e2f4fd51fc9e264106b37faa20
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-02-26 11:51:06 +0300

    Update install_deps.sh

--- patcherapk/install_deps.sh
+++ patcherapk/install_deps.sh
@@ -2,7 +2,6 @@ echo "Updating Apt Sources"
 sudo apt update -y
 echo "Updating Apt Packages"
 sudo apt upgrade -y
-echo "Installing Java Via Apt"
-sudo apt install java -y
-echo "Installing Apktool Via Apt"
-sudo apt install apktool -y
+echo "Installing Depencies Via Apt"
+sudo apt install xmlstarlet apktool java -y
+echo "Finished"
