fee0475ae020313ed0209efb137f2b802b90b02a
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-02-26 11:49:41 +0300

    Update install_deps.sh

--- patcherapk/install_deps.sh
+++ patcherapk/install_deps.sh
@@ -1,6 +1,8 @@
 echo "Updating Apt Sources"
-sudo apt update
+sudo apt update -y
 echo "Updating Apt Packages"
-sudo apt upgrade
+sudo apt upgrade -y
+echo "Installing Java Via Apt"
+sudo apt install java -y
 echo "Installing Apktool Via Apt"
-sudo apt install apktool
+sudo apt install apktool -y
