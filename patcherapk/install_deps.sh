ea81c5b3b1115fb8f9d25f3665c085a3b802d8f6
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-02-26 11:57:15 +0300

    Update install_deps.sh

--- patcherapk/install_deps.sh
+++ patcherapk/install_deps.sh
@@ -3,5 +3,5 @@ sudo apt update -y > logs/aptupdate.log
 echo "Updating Apt Packages"
 sudo apt upgrade -y > logs/aptupgrade.log
 echo "Installing Depencies Via Apt"
-sudo apt install xmlstarlet zipalign apktool java -y > logs/aptdeps.log
+sudo apt install apksigner xmlstarlet zipalign apktool java -y > logs/aptdeps.log
 echo "Finished, Please Check 3 Logs Files On 'logs' Folder to Understand For Succerful Depencies Install"
