c39e81eb82eda4ce5feb4d47e15dfe4587fc3731
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-02-26 11:53:03 +0300

    Update install_deps.sh

--- patcherapk/install_deps.sh
+++ patcherapk/install_deps.sh
@@ -1,7 +1,7 @@
 echo "Updating Apt Sources"
-sudo apt update -y
+sudo apt update -y > logs/aptupdate.log
 echo "Updating Apt Packages"
-sudo apt upgrade -y
+sudo apt upgrade -y > logs/aptupgrade.log
 echo "Installing Depencies Via Apt"
-sudo apt install xmlstarlet apktool java -y
-echo "Finished"
+sudo apt install xmlstarlet apktool java -y > logs/aptdeps.log
+echo "Finished, Please Check 3 Logs Files On 'logs' Folder to Understand For Succerful Depencies Install"
