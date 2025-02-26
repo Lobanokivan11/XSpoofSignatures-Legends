834c2e6914c9510509d7fe9119dc5067f37d6904
Lobanokivan11 <lobanokivan11@gmail.com>
2025-02-26 12:25:50 +0300

    :

--- patcherapk/install_deps.sh
+++ patcherapk/install_deps.sh
@@ -4,5 +4,5 @@ apt update -y > logs/aptupdate.log
 echo "Updating Apt Packages"
 apt upgrade -y > logs/aptupgrade.log
 echo "Installing Depencies Via Apt"
-apt install apksigner xmlstarlet zipalign apktool java -y > logs/aptdeps.log
+apt install apksigner xmlstarlet zipalign apktool openjdk-17-jdk -y > logs/aptdeps.log
 echo "Finished, Please Check 3 Logs Files On 'logs' Folder to Understand For Succerful Depencies Install"
